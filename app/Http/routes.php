f41cb9b698e5955218e7bb0312c4534da555f5ed
q262744210 <q262744210@126.com>
2017-12-04 15:55:31 +0800

    finish basic auth system

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,3 +27,12 @@ Route::resource('photos','PhotosController');
 
 Route::resource('articles','ArticlesController');
 Route::post('markdown','ArticlesController@markdown')->name('markdown');
+
+// login
+Route::get('auth/login','Auth\AuthController@getLogin');
+Route::post('auth/login','Auth\AuthController@postLogin');
+Route::get('auth/logout','Auth\AuthController@getLogout');
+// register
+
+Route::get('auth/register','Auth\AuthController@getRegister');
+Route::post('auth/register','Auth\AuthController@postRegister');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,23 @@
 				<a href="{{ route('home')}} " class="navbar-brand">Laravel Album</a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav"></ul>
+				<ul class="nav navbar-nav navbar-right">
+					<!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/auth/login') }}">Sign in</a></li>
+                        <li><a href="{{ url('/auth/register') }}">Sign up</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/auth/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Sign out</a></li>
+                            </ul>
+                        </li>
+                    @endif
+				</ul>
 				
 			</div>
 		</div>
